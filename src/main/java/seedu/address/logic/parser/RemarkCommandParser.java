422b098616f8d0dc006504cf8e46d01979e58f4d
Justinnje <e0543778@u.nus.edu>
2022-02-25 12:05:40 +0800

    Add Remark Feature to AddressBook

---
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -0,0 +1,31 @@
+package seedu.address.logic.parser;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.RemarkCommand;
+import seedu.address.logic.parser.exceptions.ParseException;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+
+public class RemarkCommandParser implements Parser<RemarkCommand> {
+    @Override
+    public RemarkCommand parse(String userInput) throws ParseException {
+        requireNonNull(userInput);
+        ArgumentMultimap argMultiMap = ArgumentTokenizer.tokenize(userInput, PREFIX_REMARK);
+
+        Index index;
+
+        try {
+            index = ParserUtil.parseIndex(argMultiMap.getPreamble());
+        } catch (IllegalValueException ive) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, RemarkCommand.MESSAGE_USAGE), ive);
+        }
+
+        String remark = argMultiMap.getValue(PREFIX_REMARK).orElse("");
+
+        return new RemarkCommand(index, remark);
+    }
+}
